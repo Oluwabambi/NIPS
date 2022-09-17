50e105a638920afb9f1aff29d815f332cb5fa228
Bambi <bawe97@gmail.com>
2022-09-17 15:33:08 +0100

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'http://102.68.170.27/nip-mini/public/index.php/api/v1',
+  CLIENT_TABLE: 'http://102.68.170.27/nip-mini/public/index.php/api/v1/clients/datatable',
+  ACCOUNT_TABLE: 'http://102.68.170.27/nip-mini/public/index.php/api/v1/accounts/datatable',
+  TRANSACTION_TABLE: 'http://102.68.170.27/nip-mini/public/index.php/api/v1/transactions/datatable',
 };
